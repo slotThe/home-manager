fa7373f640337f3f76b0567ee753ea3e4321a119
Tony Zorman <soliditsallgood@mailbox.org>
2024-02-25 14:10:31 +0100

    firefox: add containersForce flag
    Firefox, upon exit, creates the default containers.json file in place of
    the one that home-manager created. This leads to errors when switching
    to a new profile, as home-manager is careful with overwriting existing
    files. The added option toggles that behaviour.
    
    Closes: https://github.com/nix-community/home-manager/issues/4989

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -525,6 +525,17 @@ in {
               };
             };
 
+            containersForce = mkOption {
+              type = types.bool;
+              default = false;
+              description = ''
+                Whether to force replace the existing containers
+                configuration. This is recommended since Firefox will
+                replace the symlink on every launch, but note that you'll
+                lose any existing configuration by enabling this.
+              '';
+            };
+
             containers = mkOption {
               type = types.attrsOf (types.submodule ({ name, ... }: {
                 options = {
@@ -694,6 +705,7 @@ in {
 
       "${profilesPath}/${profile.path}/containers.json" =
         mkIf (profile.containers != { }) {
+          force = profile.containersForce;
           text = mkContainersJson profile.containers;
         };
 
